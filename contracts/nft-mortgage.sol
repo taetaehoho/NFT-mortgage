f65fac4b5babc260744ca533d5376680c344caec
Terry Chung <tc2886@columbia.edu>
2021-11-24 18:14:57 -0500

    basic comments for main function nftmortgage

--- contracts/nft-mortgage.sol
+++ contracts/nft-mortgage.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.5;
 
+import "./projects/nftfi/nftfi.sol"
 
-contract nft-mortage { 
+// @title Main contract for nftmortgage. This contract manages the ability to create NFT mortgages and manage Lien rights. 
+// @author @0xtaetaehoho @0xcesare
+// @notice there are 6 steps to execute an NFT mortgage. Check out our article "Economic Primitives of the Metaverse for more information."
+
+contract nftmortage is NFTfi { 
     
+
 }
