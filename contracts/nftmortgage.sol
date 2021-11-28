c932542118b6e66495f2a5c361326952d65f6f2d
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2021-11-28 15:19:01 -0500

    Update and rename nft-mortgage.sol to nftmortgage.sol

--- contracts/nftmortgage.sol
+++ contracts/nftmortgage.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.5;
 
-import "./projects/nftfi/nftfi.sol"
 
 // @title Main contract for nftmortgage. This contract manages the ability to create NFT mortgages and manage Lien rights. 
 // @author @0xtaetaehoho @0xcesare
 // @notice there are 6 steps to execute an NFT mortgage. Check out our article "Economic Primitives of the Metaverse for more information."
 
-contract nftmortage is NFTfi { 
+contract nftmortage { 
     
 
 }
