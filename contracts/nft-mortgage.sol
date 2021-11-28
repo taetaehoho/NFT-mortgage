ec1a5c5984a3dcbd87cdda0bf5289bdd98b79ef7
Terry Chung <tc2886@columbia.edu>
2021-11-28 16:05:33 -0500

    Update nft-mortgage.sol

--- contracts/nft-mortgage.sol
+++ contracts/nft-mortgage.sol
@@ -1,73 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.5;
 
-import "./projects/nftfi/nftfi.sol"
+
 
 // @title Main contract for nftmortgage. This contract manages the ability to create NFT mortgages Lien rights. 
 // @author @0xtaetaehoho @0xcesare @Jonatha26383028
 // @notice there are 6 steps to execute an NFT mortgage. Check out our article "Economic Primitives of the Metaverse for more information."
 
-contract nftmortage is NFTfi {
+contract nftmortage {
 
-// Copy EDCSA Library from
-// File: contracts/NFTfi/v1/openzeppelin/ECDSA.sol
 
-/**
- * @title Elliptic curve signature operations
- * @dev Based on https://gist.github.com/axic/5b33912c6f61ae6fd96d6c4a47afde6d
- * TODO Remove this library once solidity supports passing a signature to ecrecover.
- * See https://github.com/ethereum/solidity/issues/864
- */
-
-library ECDSA {
-    /**
-     * @dev Recover signer address from a message by using their signature
-     * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
-     * @param signature bytes signature, the signature is generated using web3.eth.sign()
-     */
-    function recover(bytes32 hash, bytes memory signature) internal pure returns (address) {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        // Check the signature length
-        if (signature.length != 65) {
-            return (address(0));
-        }
-
-        // Divide the signature in r, s and v variables
-        // ecrecover takes the signature parameters, and the only way to get them
-        // currently is to use assembly.
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            r := mload(add(signature, 0x20))
-            s := mload(add(signature, 0x40))
-            v := byte(0, mload(add(signature, 0x60)))
-        }
-
-        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-        if (v < 27) {
-            v += 27;
-        }
-
-        // If the version is correct return the signer address
-        if (v != 27 && v != 28) {
-            return (address(0));
-        } else {
-            return ecrecover(hash, v, r, s);
-        }
     }
-
-    /**
-     * toEthSignedMessageHash
-     * @dev prefix a bytes32 value with "\x19Ethereum Signed Message:"
-     * and hash the result
-     */
-    function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32) {
-        // 32 is the length in bytes of hash,
-        // enforced by the type signature above
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-    }
-}
-
-}
